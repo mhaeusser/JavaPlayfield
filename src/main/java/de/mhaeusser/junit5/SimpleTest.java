c62cbd9de778573f10549fce6f0901d8a41b6dbe
Matthias Häußer <matthias.haeusser@t-systems.com>
2018-02-20 14:49:44 +0100

    Added JUnit5 test.

---
+++ src/main/java/de/mhaeusser/junit5/SimpleTest.java
@@ -0,0 +1,11 @@
+package de.mhaeusser.junit5;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleTest {
+
+    @Test
+    void simpleTestIsPassing() {
+        org.junit.jupiter.api.Assertions.assertTrue(true);
+    }
+}

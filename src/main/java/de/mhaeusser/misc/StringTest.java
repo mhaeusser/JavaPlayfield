9cbc9bbc87aa572f2e43e898e4e033f81642df55
mhaeusser <mhaeusser@web.de>
2019-02-20 13:42:06 +0100

    Added test method.

--- src/main/java/de/mhaeusser/misc/StringTest.java
+++ src/main/java/de/mhaeusser/misc/StringTest.java
@@ -1,7 +1,20 @@
 package de.mhaeusser.misc;
 
+import org.junit.Test;
+
 public class StringTest {
 
+	@Test
+	public void stringValueOfTest() {
+		System.out.println(1);
+		System.out.println(String.valueOf(""));
+		System.out.println(2);
+		System.out.println(String.valueOf((Object) null));
+		System.out.println(3);
+		System.out.println(String.valueOf(null)); // goes to valueOf(char data[]) 
+		System.out.println(4);
+	}
+	
     public static void changeString(String string) {
         string = "changed";
 	}

d00363c9b7b677092d374663c712735cd89e56b9
Matthias Häußer <mhaeusser@web.de>
2019-07-05 20:58:02 +0200

    Adapted to Java 11.

--- Java8/src/de/mhaeusser/java8/lambda/LambdaTest2.java
+++ Java8/src/de/mhaeusser/java8/lambda/LambdaTest2.java
@@ -1,7 +1,7 @@
 package de.mhaeusser.java8.lambda;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.function.IntUnaryOperator;
 
--- Java8/src/de/mhaeusser/java8/lambda/MethodReferenceTest.java
+++ Java8/src/de/mhaeusser/java8/lambda/MethodReferenceTest.java
@@ -1,8 +1,8 @@
 package de.mhaeusser.java8.lambda;
 
 import java.util.function.IntBinaryOperator;
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MethodReferenceTest {
 

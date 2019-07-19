5115afc7f9de22b4089a156d537a33d6ad64052d
Matthias Häußer <mhaeusser@web.de>
2019-07-19 19:41:57 +0200

    Added Xor.java

---
+++ Misc/src/de/mhaeusser/misc/Xor.java
@@ -0,0 +1,12 @@
+package de.mhaeusser.misc;
+
+public class Xor {
+
+    // I know what xor means. This is just to remind me that this operator exists.
+    public static void main(String[] args) {
+        System.out.println(true ^ true);
+        System.out.println(false ^ true);
+        System.out.println(true ^ false);
+        System.out.println(false ^ false);
+    }
+}

5d2e67ebbbc826be73fe87d1f6030ff869172a88
Matthias Häußer <mhaeusser@gmail.com>
2021-02-25 16:44:22 +0100

    Add Map test

--- Java8/src/de/mhaeusser/java8/map/MapOptionalTest2.java
+++ Java8/src/de/mhaeusser/java8/map/MapOptionalTest2.java
@@ -2,23 +2,41 @@ package de.mhaeusser.java8.map;
 
 import java.util.Optional;
 
-public class MapOptionalTest {
+public class MapOptionalTest2 {
 
     public static void main(String[] args) {
+        new MapOptionalTest2().go();
+    }
+
+    private void go() {
+
+//        Optional<String> result = Optional.ofNullable(getText())
+ //               .or(() -> getText());
+
+
+                        //.map(String::toUpperCase));
+
+//                .ifPresent(id -> contactInput.setAccount(new ContactAccount().ucid(id)));
+
+
         Optional<String> opt = optString(true);
-        final Optional<Upper> upper = opt.map(Upper::new);
         System.out.println(opt);
+        final Optional<Upper> upper = opt.map(Upper::new);
         System.out.println(upper);
         upper.ifPresent(x -> System.out.println(upper.get()));
 
         Optional<String> opt2 = optString(false);
-        final Optional<Upper> upper2 = opt2.map(Upper::new);
         System.out.println(opt2);
+        final Optional<Upper> upper2 = opt2.map(Upper::new);
         System.out.println(upper2);
         upper2.ifPresentOrElse(x -> System.out.println(upper.get()), () -> System.out.println("empty"));
     }
 
     public static Optional<String> optString(boolean b) {
         return b ? Optional.of("text") : Optional.empty();
+    }
+
+    public Optional<String> getText() {
+        return Optional.of("text");
     }
 }
